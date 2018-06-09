61b103f1809d5e51c83ba8a537eff6aac5265db5
Charly Mannion <charlottemannion100@gmail.com>
2018-06-09 14:45:20 +0100

    issue with json and bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 var path = require('path');
 
+var node_dir = __dirname + '/node_modules';
+
 module.exports = {
     entry: './src/main/js/app.js',
     devtool: 'sourcemaps',
     cache: true,
+    debug: true,
     output: {
         path: __dirname,
         filename: './src/main/resources/static/built/bundle.js'
     },
     module: {
-        rules: [
+        loaders: [
             {
                 test: path.join(__dirname, '.'),
                 exclude: /(node_modules)/,
